597644585ab7112d698d734fa3b9eeb14413423e
Clayton Errington <me@claytonerrington.com>
2023-01-12 13:07:10 -0600

    update changes and workflow

--- scripts/check_alphabetical_sorting.sh
+++ scripts/check_alphabetical_sorting.sh
@@ -6,10 +6,3 @@ yq 'sort_by(.domain)' _data/sites.yml > _data/sorted.yml
 
 rm _data/sites.yml
 mv _data/sorted.yml _data/sites.yml
-
-git add .
-git diff
-git config --global user.email "github-action-bot@example.com"
-git config --global user.name "GitHub Action Bot"
-git commit -m "_data/sites.yml" -a || echo "No changes to commit"
-git push
